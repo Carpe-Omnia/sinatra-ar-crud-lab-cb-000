4229a349808cc670097d76419ed5ea8ae08efb17
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-14 21:38:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,31 +11,38 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
   get '/articles/new' do
     erb :new
   end
+
   post '/articles' do
     chad = Article.create(title: params["title"], content: params["content"])
     @articles = Article.all
     erb :index
   end
+
   get '/articles' do
     @articles = Article.all
     erb :index
   end
+
   get 'articles/:id' do
     @article = Article.find(params[:id])
     erb :show
   end
+
   get 'articles/:id/edit' do
     @article = Article.find(params[:id])
     erb :edit
   end
+
   patch '/articles/:id' do
     article = Article.find(params[:id])
     article.update(title: params["title"], content: params["content"])
     erb :index
   end
+
   delete '/articles/:id'
     article = Article.find(params[:id])
     article.delete
