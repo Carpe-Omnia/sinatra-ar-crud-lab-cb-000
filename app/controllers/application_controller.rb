00247f0ef640bacc0969713eb320218fe4e56b9f
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-14 22:17:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,46 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    erb :new
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    chad = Post.create(name: params["name"], content: params["content"])
+    @articles = Post.all
+    erb :posts
+  end
+
+  get '/posts' do
+    @articles = Post.all
+    erb :posts
+  end
+
+  get '/posts/:id' do
+    @article = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @article = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    article = Post.find(params[:id])
+    article.update(name: params["name"], content: params["content"])
+    @articles = Post.all
+    erb :posts
+  end
+
+  delete '/posts/:id' do
+    article = Post.find(params[:id])
+    article.delete
+    @articles = Post.all
+    erb :posts
+  end
+
 end
